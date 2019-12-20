8e05fcd8af770b620eb4015a8e3659d3bcc28804
fypj-birdnest <57695428+fypj-birdnest@users.noreply.github.com>
2019-12-20 10:39:44 +0800

    20/12

--- app/src/main/java/com/example/newtest/BatchProcessing.kt
+++ app/src/main/java/com/example/newtest/BatchProcessing.kt
@@ -1,7 +1,10 @@
 package com.example.newtest
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_batchprocessing.*
+import java.util.*
 
 class BatchProcessing : AppCompatActivity(){
 
@@ -9,6 +12,20 @@ class BatchProcessing : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_batchprocessing)
 
-    }
+        //Calendar
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
 
+        //button click to show DatePickerDialog
+        pickDateBtn.setOnClickListener {
+            val dpd = DatePickerDialog(this, DatePickerDialog.OnDateSetListener{view, mYear, mMonth, mDay ->
+                //set to TextView
+                dateTv.setText("" + mDay + "/" + (mMonth+1) + "/" + mYear)
+            }, year, month, day )
+            //show dialog
+            dpd.show()
+        }
+    }
 }

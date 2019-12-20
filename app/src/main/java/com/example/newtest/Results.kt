6ef64b85a4c2a23786032b8b7017508dd6662130
fypj-birdnest <fypj.birdnest@gmail.com>
2019-12-20 10:51:14 +0800

    merging

--- app/src/main/java/com/example/newtest/Results.kt
+++ app/src/main/java/com/example/newtest/Results.kt
@@ -14,6 +14,10 @@ import com.google.firebase.functions.FirebaseFunctionsException
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_results.*
 import java.io.Serializable
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import com.google.gson.Gson
 
 
 class Results : AppCompatActivity(){
@@ -27,45 +31,67 @@ class Results : AppCompatActivity(){
         val db = FirebaseFirestore.getInstance()
         val firefunc = FirebaseFunctions.getInstance()
 
-        val tQr = intent.extras?.get("tQr") as QrString
+//        val tQr = intent.extras?.get("tQr") as QrString
+//
+//        brandValue.text = tQr.brand
+//        countryOriginValue.text = tQr.country
+//        acidityLevelValue.text = tQr.acidity
 
-        brandValue.text = tQr.brand
+        val tQr = intent.getStringExtra("tQr")
+        Log.d("the tqr",tQr)
+        brandValue.text = tQr
+
+        db.collection("qr").get().addOnSuccessListener { result ->
+            for (document in result) {
+//                Log.d("theResult", "${document.id} => ${document.data}")
+                if(tQr == document.getString("code")){
+                    brandValue.text = document.getString("brand")
+                    countryOriginValue.text = document.getString("country")
+                    acidityLevelValue.text = document.getString("acidity")
+
+                    //insert graph code here
 
 
 
+                }
+            }
+
+        }.addOnFailureListener { exception ->
+            Log.w("GetDocError", "Error getting documents.", exception)
+        }
+
+        val text:String = "text"
         val data = hashMapOf(
-            "text" to text,
+            "text"  to text,
             "push" to true
         )
 
-        firefunc.getHttpsCallable("brandState").call()
-                .addOnCompleteListener { task ->
-                    if (!task.isSuccessful)
-                    {
-                        db.collection("graphImages").get().addOnSuccessListener { result ->
-                            for (document in result) {
-                                //Log.d("theResult", "${document.id} => ${document.data}")
-                                if("brandState5" == document.id){
-                                    Picasso
-                                        .get()
-                                        .load(document.getString("url"))
-                                        .into(graph)
-                                }
-                            }
-                        }.addOnFailureListener { exception ->
-                            Log.w("GetDocError", "Error getting documents.", exception)
-                        }
-
-                        val e = task.exception
-                        if (e is FirebaseFunctionsException)
-                        {
-                            Log.d("error in calling func", "Result3: " + e.code + e.details)
-                        }
-                    }
-                    return@addOnCompleteListener
-                }
-
-
+//        firefunc.getHttpsCallable("brandState").call(data)
+//                .addOnCompleteListener { task ->
+//                    if (!task.isSuccessful)
+//                    {
+//                        db.collection("graphImages").get().addOnSuccessListener { result ->
+//                            for (document in result) {
+//                                //Log.d("theResult", "${document.id} => ${document.data}")
+//                                if("brandState5" == document.id){
+//                                    Picasso
+//                                        .get()
+//                                        .load(document.getString("url"))
+//                                        .into(graph)
+//                                }
+//                            }
+//                        }.addOnFailureListener { exception ->
+//                            Log.w("GetDocError", "Error getting documents.", exception)
+//                        }
+//
+//                        val e = task.exception
+//                        if (e is FirebaseFunctionsException)
+//                        {
+//                            Log.d("error in calling func", "Result3: " + e.code + e.details)
+//                        }
+//                    }
+//                    return@addOnCompleteListener
+//                }
 
     }
 }
